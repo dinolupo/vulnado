178bb1599d4e198f17ded000aedde446093415e1
dino lupo <dino.lupo@gmail.com>
2025-04-03 12:13:27 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -3,12 +3,13 @@ package com.scalesec.vulnado;
 import java.util.logging.Logger;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+private Cowsay() {}
   private Cowsay() {}
 public class Cowsay {
   public static String run(String input) {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
+    Logger logger = Logger.getLogger(Cowsay.class.getName());
+logger.info("Executing command with ProcessBuilder");
+    String cmd = "/usr/games/cowsay '" + input.replaceAll("[\\\\"'\\\\]", "") + "'";
     Logger logger = Logger.getLogger(Cowsay.class.getName());
     processBuilder.command("bash", "-c", cmd); // Ensure PATH is sanitized and validated
 
@@ -21,9 +22,9 @@ public class Cowsay {
       String line;
       while ((line = reader.readLine()) != null) {
         output.append(line + "\n");
-      }
-    } catch (Exception e) {
       logger.warning("Debug feature activated: " + e.getMessage());
+    } catch (Exception e) {
+      logger.severe("An error occurred while executing the command: " + e.getMessage());
     }
     return output.toString();
   }
