c02c0456c32cc677d057fea5f3d2236bf498606b
dino lupo <dino.lupo@gmail.com>
2025-04-03 12:18:13 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LinksController.java

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,10 +1,7 @@
 package com.scalesec.vulnado;
 
-import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
 import java.util.List;
 import java.io.IOException;
-
 
 @RestController
 @EnableAutoConfiguration
@@ -12,9 +9,9 @@ public class LinksController {
   @RequestMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
     return LinkLister.getLinks(url);
-  }
+  @RequestMapping(value = "/links", produces = "application/json", method = RequestMethod.GET)
   @RequestMapping(value = "/links-v2", produces = "application/json")
   List<String> linksV2(@RequestParam String url) throws BadRequest{
     return LinkLister.getLinksV2(url);
-  }
+  @RequestMapping(value = "/links-v2", produces = "application/json", method = RequestMethod.GET)
 }
