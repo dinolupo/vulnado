5f5fa55e20f09d6fd920b9bd6a39b9b1cbee3328
dino lupo <dino.lupo@gmail.com>
2025-04-03 12:24:43 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Comment.java

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -1,6 +1,5 @@
 package com.scalesec.vulnado;
 
-import java.sql.*;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
@@ -11,7 +10,7 @@ public class Comment {
   private String username;
   private Timestamp createdOn;
   private String body;
-
+public Comment(String id, String username, String body, Timestamp createdOn) {
   public Comment(String id, String username, String body, Timestamp createdOn) {
     this.id = id;
     this.username = username;
@@ -23,7 +22,7 @@ public class Comment {
     long time = new Date().getTime();
     Timestamp timestamp = new Timestamp(time);
     Comment comment = new Comment(UUID.randomUUID().toString(), username, body, timestamp);
-    try {
+    if (comment.commit()) {
       if (Boolean.TRUE.equals(comment.commit())) {
         return comment;
       } else {
@@ -33,14 +32,14 @@ public class Comment {
       throw new ServerError(e.getMessage());
     }
   }
-
+public static List<Comment> fetchAll() {
   public static List<Comment> fetchAll() {
-    Statement stmt = null;
+    List<Comment> comments = new ArrayList<>();
     List<Comment> comments = new ArrayList<>();
     try {
-      Connection cxn = Postgres.connection();
+      try (Connection cxn = Postgres.connection()) {
       try (Statement stmt = cxn.createStatement()) {
-
+String query = "SELECT id, username, body, createdOn FROM comments;";
       String query = "SELECT id, username, body, created_on FROM comments;";
       ResultSet rs = stmt.executeQuery(query);
       while (rs.next()) {
@@ -52,10 +51,9 @@ public class Comment {
         comments.add(c);
       }
       cxn.close();
-    } catch (Exception e) {
-      Logger logger = Logger.getLogger(Comment.class.getName());
+    Logger logger = Logger.getLogger(Comment.class.getName());
       logger.severe(e.getClass().getName() + ": " + e.getMessage());
-    } finally {
+      logger.severe(e.getClass().getName() + ": " + e.getMessage());
     }
   }
 
@@ -67,19 +65,18 @@ public class Comment {
       pStatement.setString(1, id);
       return 1 == pStatement.executeUpdate();
     } catch(Exception e) {
-      e.printStackTrace();
     } finally {
-    }
   }
 
   private Boolean commit() throws SQLException {
     String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?,?,?,?)";
-    Connection con = Postgres.connection();
+    try (Connection con = Postgres.connection()) {
       try (PreparedStatement pStatement = con.prepareStatement(sql)) {
     pStatement.setString(1, this.id);
     pStatement.setString(2, this.username);
     pStatement.setString(3, this.body);
-    pStatement.setTimestamp(4, this.created_on);
+    pStatement.setTimestamp(4, this.createdOn);
     return 1 == pStatement.executeUpdate();
   }
+}
 }
